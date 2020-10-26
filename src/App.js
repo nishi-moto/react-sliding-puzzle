1b37ee00565bd2ddcc8c6e87601556c719322a14
Alessandra <alessandra@nishimoto.me>
2020-10-26 20:20:25 +0100

    working game engine

--- src/App.js
+++ src/App.js
@@ -1,49 +1,77 @@
 import React, { Component } from 'react';
 import './App.css';
-import Board from './Board';
 import Tile from './Tile';
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      tilesPosition : [],
-    };
     this.size = 3;
     this.length = this.size * this.size;
     this.puzzle = [];
-    this.empty = this.length - 1;
     this.percentage = `${Math.floor(100 / this.size)}%`;
+    this.state = {
+      tilesPosition: [],
+      empty : this.length - 1
+    };
   }
-
 
   initPuzzle = () => {
-    for (let i = 0; i < this.length-1; i++) {
+    for (let i = 0; i < this.length - 1; i++) {
       this.puzzle[i] = i;
-    } 
+    }    
     this.puzzle = this.puzzle.sort(() => Math.random() - 0.5);
-    this.puzzle[this.empty] = null;
-    return this.populatePuzzle();
+    this.puzzle[this.state.empty] = null;
+    this.setState({ ...{tilesPosition: this.puzzle}});
   }
 
-  populatePuzzle = () => {
-    console.log(this.puzzle);
-    return this.puzzle.map((cell, i) => (
-      <div className="Board-cell" style={{ width: this.percentage, 'padding-bottom': this.percentage }}>
-        <Tile correctPosition={i < this.length - 1 ? i : null} currentPosition={cell}/>
+  componentDidMount = () => {
+    this.initPuzzle();
+  }
+  
+  renderPuzzle = () => {
+    return this.state.tilesPosition.map((cell, i) => (
+      <div className="Board-cell" style={{ width: this.percentage, paddingBottom: this.percentage }}>
+        <Tile correctPosition={i} currentPosition={cell} moveHandler={this.move} key={i}/>
       </div>
     ))
   }
+ 
+  line = (tile) => {
+    return Math.floor(tile / this.size);
+  }
 
+  swapTiles = (from, to) => {
+    const puzzle = this.state.tilesPosition;
+    [puzzle[from], puzzle[to]] = [puzzle[to], puzzle[from]];
+    this.setState({
+      tilesPosition: puzzle,
+      empty : from
+    });
+  }
+
+  canMove = (tile) => {
+    const emptyLine = this.line(this.state.empty, this.size);
+    const tileLine = this.line(tile, this.size);
+
+    return Math.abs(
+      Math.abs(emptyLine - tileLine) - (Math.floor(Math.abs(tile - this.state.empty) / this.size))
+    ) === 0;
+  }
+
+  move = (tile) => {
+    if (!this.canMove(tile)) {
+      return;
+    }
+    this.swapTiles(tile, this.state.empty);
+  }
 
   render() {
     return (
       <div className="App">
         <h1>Sliding Puzzle</h1>
-        <div className="Board">{this.initPuzzle()}</div>
+         <div className="Board">{this.renderPuzzle()}</div> 
       </div>
     )
-
   }
 
 }
--- src/Tile.js
+++ src/Tile.js
@@ -1,11 +1,14 @@
 function Tile(props) {
-  // props.correctPostition !== null ? props.correctPostition+1 : null
-  return (
-    
-    <div className="Tile">
-     current position: { props.currentPosition !== null ? props.currentPosition+1 : null}
-     <br/>
-     correct position: { props.correctPosition !== null ? props.correctPosition+1 : null}
+
+  const tileOnClickHandler = () => {
+    props.moveHandler(props.correctPosition);
+  }
+
+  return (  
+    <div className="Tile"  onClick={tileOnClickHandler}>
+      current position: { props.currentPosition !== null ? props.currentPosition : null}
+      <br/>
+      correct position: { props.correctPosition !== null ? props.correctPosition : null}
     </div>
   );
 }
